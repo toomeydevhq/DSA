c6b10046b81d42e0773fdb0cac0dc287c5005d73
Stephen Toomey <stephentoomey406@gmail.com>
2026-01-13 21:19:59 +0100

    practice and implement quick sort algorithm

---
+++ quickSort/quickSort.js
@@ -0,0 +1,37 @@
+// //function quickSort(arr) {
+//   if (arr.length <= 1) {
+//     return arr;
+//   }
+
+//   const pivot = arr[Math.floor(arr.length / 2)];
+//   const left = [];
+//   const right = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < pivot) {
+//       left.push(arr[i]);
+//     } else if (arr[i] > pivot) {
+//       right.push(arr[i]);
+//     }
+//   }
+
+//   return [...quickSort(left), pivot, ...quickSort(right)];
+// }
+
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  } else {
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < pivot) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)];
+  }
+}
